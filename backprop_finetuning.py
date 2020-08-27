fixed dataset size to reduce numerical overflow problem

backprop_finetuning.py
@@ -31,7 +31,7 @@ dataset_name = "landcover_large"
 dataset_dir = os.path.join(base_dir, "data/" + dataset_name)
 
 experiment_name = "backprop_single_point_finetuning"
-model_name = "best_model_30_validation_accuracy=0.9409.pt"
+model_name = "best_model_9_validation_accuracy=0.8940.pt"
 model_path = os.path.join(base_dir, "logs/" + dataset_name + "/" + model_name)
 log_dir = os.path.join(base_dir, "logs/" + dataset_name + "_" + experiment_name)
 
@@ -57,11 +57,11 @@ device = maybe_get_cuda_device()
 
 # Determine model and training params.
 params = {
-    'max_epochs': 5,
+    'max_epochs': 10,
     'n_classes': 4,
     'in_channels': 4,
     'depth': 5,
-    'learning_rate': 0.01,
+    'learning_rate': 0.001,
     'log_steps': 1,
     'save_top_n_models': 4
 }
@@ -73,23 +73,17 @@ model = UNet(in_channels = params['in_channels'],
 model.load_state_dict(torch.load(model_path))
 
 model.to(device)
-criterion = nn.CrossEntropyLoss()
+
+# Create Trainer or Evaluators
+criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), 
                              lr=params['learning_rate'])
 
-
 # Determine metrics for evaluation.
-train_metrics = {
+metrics = {
         "accuracy": Accuracy(), 
         "loss": Loss(criterion),
         "mean_iou": mIoU(ConfusionMatrix(num_classes = params['n_classes'])),
-        }
-
-validation_metrics = {
-        "accuracy": Accuracy(), 
-        "loss": Loss(criterion),
-        "mean_iou": mIoU(ConfusionMatrix(num_classes = params['n_classes'])),
-
 }
 
 def backprop_step(engine, batch):
@@ -102,16 +96,16 @@ def backprop_step(engine, batch):
     loss.backward()
     optimizer.step()
     return loss.item()
-  
+ 
+
 # Create Trainer or Evaluators
 trainer = Engine(backprop_step)
-train_evaluator = create_supervised_evaluator(model, metrics=train_metrics, device=device)
-validation_evaluator = create_supervised_evaluator(model, metrics=validation_metrics, device=device)
+train_evaluator = create_supervised_evaluator(model, metrics=metrics, device=device)
+validation_evaluator = create_supervised_evaluator(model, metrics=metrics, device=device)
 
 trainer.logger = setup_logger("Trainer")
 train_evaluator.logger = setup_logger("Train Evaluator")
 validation_evaluator.logger = setup_logger("Validation Evaluator")
-
 
 
 # Tensorboard Logger setup below based on pytorch ignite example

dropout_finetuning.py
@@ -62,10 +62,10 @@ params = {
     'n_classes': 4,
     'in_channels': 4,
     'depth': 5,
-    'learning_rate': 0.01,
+    'learning_rate': 0.001,
     'log_steps': 1,
     'save_top_n_models': 4,
-    'num_children': 20
+    'num_children': 30
 }
 
 clear_cuda()    
@@ -73,22 +73,16 @@ model = UNet(in_channels = params['in_channels'],
              n_classes = params['n_classes'],
              depth = params['depth'])
 model.load_state_dict(torch.load(model_path))
-criterion = nn.CrossEntropyLoss()
+# Create Trainer or Evaluators
+criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), 
                              lr=params['learning_rate'])
 
 # Determine metrics for evaluation.
-train_metrics = {
+metrics = {
         "accuracy": Accuracy(), 
         "loss": Loss(criterion),
         "mean_iou": mIoU(ConfusionMatrix(num_classes = params['n_classes'])),
-        }
-
-validation_metrics = {
-        "accuracy": Accuracy(), 
-        "loss": Loss(criterion),
-        "mean_iou": mIoU(ConfusionMatrix(num_classes = params['n_classes'])),
-
 }
 
 for batch in train_loader:
@@ -111,7 +105,7 @@ for layer in drop_out_layers:
     model.load_state_dict(torch.load(model_path))
 
     model.to(device)
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.NLLLoss()
     optimizer = torch.optim.Adam(model.parameters(), 
                                  lr=params['learning_rate'])
     
@@ -144,6 +138,7 @@ for layer in drop_out_layers:
             batch_y = batch_y.to(device)
             loss = sys.float_info.max
             for i in range(params['num_children']):
+                model.zero_grad()
                 child_vec = evolver.spawn_child()
                 child_mask = mask_from_vec(child_vec, size)
                 model.set_dropout_masks({layer_name: torch.tensor(child_mask, dtype=torch.float32).to(device)})
@@ -155,13 +150,11 @@ for layer in drop_out_layers:
                     current_loss = sys.float_info.max
                 else:
                     current_loss = 1.0 / current_loss
-                
-                print("Current", current_loss)
+
                 evolver.add_child(child_vec, current_loss)
                 
             priority, best_child = evolver.get_best_child()
-            print("Best", priority)
-            best_mask = mask_from_vec(child_vec, size)
+            best_mask = mask_from_vec(best_child, size)
             model.set_dropout_masks({layer_name: torch.tensor(best_mask, dtype=torch.float32).to(device)})
             return loss
 

landcover_dataloader.py
@@ -317,7 +317,7 @@ def get_landcover_dataloader(dataset_dir: str,
 def get_landcover_dataloaders(dataset_dir: str,
                               partition_types: Iterable[utils.PartitionType],
                               dataloader_params: Dict[str, Any],
-                              force_create_dataset: bool = False) -> Iterable[torch.utils.data.DataLoader]:
+                              force_create_dataset: bool = True) -> Iterable[torch.utils.data.DataLoader]:
     """Gets a list pytorch DataLoaders for landcover data.
 
     Args:

training.py
@@ -33,7 +33,7 @@ log_dir = os.path.join(base_dir, "logs/" + dataset_name)
 dataloader_params = {
     'batch_size': 16,
     'shuffle': True,
-    'num_workers': 6,
+    'num_workers': 4,
     'pin_memory': True}
 
 partition_types = [PartitionType.TRAIN, PartitionType.VALIDATION, 
@@ -41,22 +41,20 @@ partition_types = [PartitionType.TRAIN, PartitionType.VALIDATION,
 data_loaders = get_landcover_dataloaders(dataset_dir, 
                                          partition_types,
                                          dataloader_params,
-                                         force_create_dataset=False)
+                                         force_create_dataset=True)
 train_loader = data_loaders[0]
 validation_loader = data_loaders[1]
-finetuning_loader = data_loaders[2]
-test_loader = data_loaders[3]
 
 # Get GPU device if available.
 device = maybe_get_cuda_device()
 
 # Determine model and training params.
 params = {
-    'max_epochs': 30,
+    'max_epochs': 10,
     'n_classes': 4,
     'in_channels': 4,
     'depth': 5,
-    'learning_rate': 0.01,
+    'learning_rate': 0.001,
     'log_steps': 1,
     'save_top_n_models': 4
 }
@@ -67,28 +65,21 @@ model = UNet(in_channels = params['in_channels'],
              depth = params['depth'])
 
 model.to(device)
-criterion = nn.CrossEntropyLoss()
+criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), 
                              lr=params['learning_rate'])
 
 # Determine metrics for evaluation.
-train_metrics = {
+metrics = {
         "accuracy": Accuracy(), 
         "loss": Loss(criterion),
         "mean_iou": mIoU(ConfusionMatrix(num_classes = params['n_classes'])),
-        }
-
-validation_metrics = {
-        "accuracy": Accuracy(), 
-        "loss": Loss(criterion),
-        "mean_iou": mIoU(ConfusionMatrix(num_classes = params['n_classes'])),
-
 }
 
 # Create Trainer or Evaluators
 trainer = create_supervised_trainer(model, optimizer, criterion, device=device)
-train_evaluator = create_supervised_evaluator(model, metrics=train_metrics, device=device)
-validation_evaluator = create_supervised_evaluator(model, metrics=validation_metrics, device=device)
+train_evaluator = create_supervised_evaluator(model, metrics=metrics, device=device)
+validation_evaluator = create_supervised_evaluator(model, metrics=metrics, device=device)
 
 trainer.logger = setup_logger("Trainer")
 train_evaluator.logger = setup_logger("Train Evaluator")
@@ -129,6 +120,18 @@ for tag, evaluator in [("training", train_evaluator), ("validation", validation_
 tb_logger.attach_opt_params_handler(trainer, 
                                     event_name=Events.ITERATION_COMPLETED(every=params['log_steps']), 
                                     optimizer=optimizer)
+
+tb_logger.attach(trainer, log_handler=WeightsScalarHandler(model), 
+                 event_name=Events.ITERATION_COMPLETED(every=params['log_steps']))
+
+tb_logger.attach(trainer, log_handler=WeightsHistHandler(model), 
+                 event_name=Events.EPOCH_COMPLETED(every=params['log_steps']))
+
+tb_logger.attach(trainer, log_handler=GradsScalarHandler(model), 
+                 event_name=Events.ITERATION_COMPLETED(every=params['log_steps']))
+
+tb_logger.attach(trainer, log_handler=GradsHistHandler(model), 
+                 event_name=Events.EPOCH_COMPLETED(every=params['log_steps']))
 
 model_checkpoint = ModelCheckpoint(
     log_dir,

unet.py
@@ -70,10 +70,7 @@ class UNet(nn.Module):
         # output classes.
         prev_layer_channels = out_channels
         out_channels = int(prev_layer_channels / scale_factor)
-        self.drop_layers.append(DropoutFinetuning("end"))
-        self.conv1d = nn.Sequential(
-                nn.Conv2d(prev_layer_channels, n_classes, kernel_size=1),
-                self.drop_layers[-1])
+        self.conv1d = nn.Conv2d(prev_layer_channels, n_classes, kernel_size=1)
         
     def forward(self, x):
         """Forward pass for the UNet performing both contraction, contraction and skip connections."""
@@ -95,8 +92,9 @@ class UNet(nn.Module):
             else:
                 # Dropout up layer.
                 out = up(out)
-
-        return self.conv1d(out)
+        
+        # Calculates softmax since pytorch expects 0 - 1 bounded output.
+        return F.log_softmax(self.conv1d(out), dim=1)
 
     def set_dropout_masks(self, masks: Dict[str, torch.Tensor]):
         """Sets the dropout mask matrices to be applied to multiple layers of the UNet.